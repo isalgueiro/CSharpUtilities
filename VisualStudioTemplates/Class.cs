06d2081581fb9d744c10d439d2f3ac76d1d06000
isalgueiro <isalgueiro@gmail.com>
2015-01-20 10:10:31 +0100

    Template for new class
    StyleCop compilant template for Visual Studio. It adds a file header,
    default constructor and regions for common elements in correct order

---
+++ VisualStudioTemplates/Class.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="$itemname$" company="$registeredorganization$">
+//     Copyright (c) $year$ $registeredorganization$. 
+//     All rights reserved.
+// </copyright>
+// <author>$username$</author>
+//-----------------------------------------------------------------------
+namespace $rootnamespace$
+{
+	using System;
+	using System.Collections.Generic;
+	$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+	$endif$using System.Text;
+	$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+	$endif$
+
+    public class $safeitemrootname$
+    {
+		#region Member Variables
+        #endregion
+ 
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="$safeitemrootname$"/> class
+        /// </summary>
+        public $safeitemrootname$()
+        {
+        }
+        #endregion
+ 
+        #region Events
+        #endregion
+
+        #region Enums
+        #endregion
+
+        #region Properties
+        #endregion
+ 
+        #region Methods
+        #endregion		
+    }
+}
